6194c0b5f40aa5e7f69b0946f196411210c0ecd7
somessii <somessii@163.com>
2019-04-17 10:43:58 +0800

    lb

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel lb
+                    Laravel lbs
                 </div>
 
                 <div class="links">
