1c4dd755b4969527565a599acc8ff31533841140
somessii <somessii@163.com>
2019-04-17 11:03:33 +0800

    change envoy2

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,6 @@
 @servers(['web-1' => 'root@192.168.23.51', 'web-2' => 'root@192.168.23.52'])
 
-@task('deploy', ['on' => ['web-1', 'web-2'], 'confirm' => true)
+@task('deploy', ['on' => ['web-1', 'web-2'], 'confirm' => true])
     cd /var/www/html/lblaravel
     git pull origin master
     composer install --no-dev
