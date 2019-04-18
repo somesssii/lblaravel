6aec3aab4f4d1bdd855230ddbd69943fcaedfb87
somessii <somessii@163.com>
2019-04-18 10:21:03 +0800

    one

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,6 @@
 @servers(['web-1' => 'root@192.168.23.51', 'web-2' => 'root@192.168.23.52'])
 
-@task('somessii', ['on' => ['web-1', 'web-2'], 'parallel' => true])
+@task('deploy', ['on' => ['web-1', 'web-2'], 'parallel' => true])
     cd /var/www/lblaravel
     git pull origin master
     composer install --no-dev
